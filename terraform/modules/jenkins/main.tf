e6248685835b6554d2edfa7137406d417709fb0e
De'Jour Ford <dejourford@yahoo.com>
2026-06-29 23:21:23 -0500

    add aws cli and docker restart command

--- terraform/modules/jenkins/main.tf
+++ terraform/modules/jenkins/main.tf
@@ -95,19 +95,24 @@ resource "aws_instance" "jenkins" {
   associate_public_ip_address = true
 
   user_data = <<-EOF
-              #!/bin/bash
-              apt-get update -y
-              apt-get install -y docker.io
-              systemctl start docker
-              systemctl enable docker
-              docker run -d \
-                --name jenkins \
-                -p 8080:8080 -p 50000:50000 \
-                -v jenkins_home:/var/jenkins_home \
-                -v /var/run/docker.sock:/var/run/docker.sock \
-                jenkins/jenkins:lts
-              EOF
+            #!/bin/bash
+            apt-get update -y
+            apt-get install -y docker.io unzip curl
+            systemctl start docker
+            systemctl enable docker
 
+            curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+            unzip awscliv2.zip
+            ./aws/install
+
+            docker run -d \
+              --name jenkins \
+              --restart unless-stopped \
+              -p 8080:8080 -p 50000:50000 \
+              -v jenkins_home:/var/jenkins_home \
+              -v /var/run/docker.sock:/var/run/docker.sock \
+              jenkins/jenkins:lts
+            EOF
   tags = {
     Name = "${var.project}-${var.environment}-jenkins-ec2"
   }
